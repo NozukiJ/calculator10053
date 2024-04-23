9419afbe7f4c0916381bc250c85acd80f16c701b
NozukiJ <junichi.nozuki@pathoslogos.co.jp>
2024-04-23 11:18:30 +0900

    confirmation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,4 @@
-// @angular/core/testingからTestBedを、
-// @angular/router/testingからRouterTestingModuleをインポートしています。
-// これらは、Angularのテストユーティリティとテスト用ルーターモジュールです。
+
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 // AppComponentをインポートしています。
